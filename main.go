87f6c4c1e07aa53527d8d9f5206a95207758a10b
Your Name <you@example.com>
2015-09-26 19:30:40 +0800

    update

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
-	"net"
 	"net/http"
+	"net/url"
+	"reflect"
 	"strconv"
 	"strings"
 	"time"
@@ -20,8 +22,7 @@ type ParallelRequest struct {
 type ParallelResponse struct {
 	id           int64
 	host         string
-	code         int
-	content      []byte
+	err          error
 	httpResponse *http.Response
 }
 
@@ -51,48 +52,69 @@ func NewParallelRequest(req *http.Request) *ParallelRequest {
 	return &request
 }
 
-func Do(request *ParallelRequest, globalTimeout time.Duration) (http.Response, string) {
+/**
+* @brief 重设Request,
+*
+* @param http.Request
+* @param
+* @param error
+*
+* @return
+ */
+func ResetRequest(request *http.Request, hostAddress string) (*http.Request, error) {
+	newUrl, err := url.Parse(hostAddress + request.RequestURI)
+	// 清除RequestURI
+	request.RequestURI = ""
+	// 重新设置地址
+	request.URL = newUrl
+	return request, err
+}
+
+/**
+* @brief 发送请求
+*
+* @param ParallelRequest
+* @param
+* @param string
+*
+* @return
+ */
+
+func Do(request *ParallelRequest, globalTimeout time.Duration) (*http.Response, string) {
 	resultQueue := make(chan ParallelResponse)
-	body, _ := ioutil.ReadAll(request.httpRequest.Body)
 	for index, _ := range request.hosts {
-		go func(address string, httpRequest http.Request) {
+		go func(hostAddress string, httpRequest http.Request) {
 			httpClient := &http.Client{
-				Transport: &http.Transport{
-					Dial: func(netw, addr string) (net.Conn, error) {
-						c, err := net.DialTimeout(netw, addr, globalTimeout)
-						if err != nil {
-							return nil, err
-						}
-						deadline := time.Now().Add(2 * globalTimeout)
-						c.SetDeadline(deadline)
-						return c, nil
-					},
-					ResponseHeaderTimeout: globalTimeout,
-				},
+				//	Transport: &http.Transport{
+				//		Dial: func(netw, addr string) (net.Conn, error) {
+				//			c, err := net.DialTimeout(netw, addr, globalTimeout)
+				//			if err != nil {
+				//				return nil, err
+				//			}
+				//			deadline := time.Now().Add(2 * globalTimeout)
+				//			c.SetDeadline(deadline)
+				//			return c, nil
+				//		},
+				//		ResponseHeaderTimeout: globalTimeout,
+				//	},
+				Timeout: globalTimeout,
 			}
 			// 转发 Request
-			rebuildRequest, err := http.NewRequest(httpRequest.Method, address+httpRequest.RequestURI, strings.NewReader(string(body)))
-			if err == nil {
-				// httpClient := &http.Client{}
-				log.Print("redirect to ", rebuildRequest.Host, rebuildRequest.URL)
-				response, err := httpClient.Do(rebuildRequest)
-				if err == nil {
-					code := response.StatusCode
-					content, err := ioutil.ReadAll(response.Body)
-					response.Body.Close()
-					if err == nil {
-						resultQueue <- ParallelResponse{request.id, address, code, content, response}
-						return
-					} else {
-						log.Print(err.Error())
-					}
-				} else {
-					log.Print(err.Error())
-				}
-				resultQueue <- ParallelResponse{request.id, address, 500, []byte("{}"), response}
-			} else {
-				resultQueue <- ParallelResponse{request.id, address, 500, []byte("{}"), nil}
+			rebuiltRequest, err := ResetRequest(&httpRequest, hostAddress)
+			if err != nil {
+				log.Println("fail to rebuild request: ", err.Error())
+				resultQueue <- ParallelResponse{request.id, hostAddress, err, nil}
+				return
 			}
+			// httpClient := &http.Client{}
+			log.Print("redirect to ", rebuiltRequest.Host, rebuiltRequest.URL)
+			response, err := httpClient.Do(rebuiltRequest)
+			if err != nil {
+				log.Print("fail to send request: ", err.Error())
+				resultQueue <- ParallelResponse{request.id, hostAddress, err, response}
+				return
+			}
+			resultQueue <- ParallelResponse{request.id, hostAddress, err, response}
 		}(request.hosts[index], request.httpRequest)
 	}
 
@@ -104,17 +126,25 @@ func Do(request *ParallelRequest, globalTimeout time.Duration) (http.Response, s
 
 	resultCount := 0
 	keepWaiting := true
-	allResponse := make([]string, 0)
+	responseCollection := make([]string, 0)
 	var chosenResponse *http.Response = nil
 	for keepWaiting {
 		select {
 		case result := <-resultQueue:
-			fmt.Println("get Response")
-			resultCount++
-			allResponse = append(allResponse, strings.Trim(string(result.content), "\n"))
-			if chosenResponse == nil && result.code == 200 {
-				chosenResponse = result.httpResponse
+			fmt.Println("get Response from ", result.id)
+			content := "{}"
+			if result.err != nil {
+				// TODO. 造一个统一的错误数据结构
+				content = fmt.Sprintf("{\"error\":\"%v\"}", result.err.Error())
+			} else if body, err := ioutil.ReadAll(result.httpResponse.Body); err == nil {
+				content = strings.Trim(string(body), "\n")
+				// 随便选一个Response作为最终的header
+				if chosenResponse == nil {
+					chosenResponse = result.httpResponse
+				}
 			}
+			responseCollection = append(responseCollection, content)
+			resultCount++
 		case <-timeoutNotifier:
 			keepWaiting = false
 		}
@@ -122,20 +152,20 @@ func Do(request *ParallelRequest, globalTimeout time.Duration) (http.Response, s
 			keepWaiting = false
 		}
 	}
-	combinedResponse := ""
-	if len(allResponse) > 1 {
-		combinedResponse = strings.Join(allResponse, ",")
-	} else if len(allResponse) == 1 {
-		combinedResponse = allResponse[0]
-	}
+	combinedResponse := strings.Join(responseCollection, ",")
 	combinedResponse = "{ response: [" + combinedResponse + "] }"
-	if chosenResponse == nil {
-		chosenResponse = &http.Response{}
-	}
-	return *chosenResponse, combinedResponse
+	return chosenResponse, combinedResponse
 }
 
-func HelloServer(w http.ResponseWriter, req *http.Request) {
+type Router struct {
+	routerMap map[string]func(w http.ResponseWriter, req *http.Request)
+}
+
+func (router *Router) InitRouterTable() {
+
+}
+
+func Redirect(w http.ResponseWriter, req *http.Request) {
 	log.Print("Receive: ", req.Host, req.URL)
 	log.Print("Redirecting...")
 	request := NewParallelRequest(req)
@@ -150,15 +180,52 @@ func HelloServer(w http.ResponseWriter, req *http.Request) {
 	log.Print("Done")
 }
 
+func (router *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	Redirect(w, req)
+}
+
+/**
+* @brief 命令行参数
+*
+* @return
+ */
+
+type ServerConfigure struct {
+	host         string
+	port         string
+	readTimeout  int
+	writeTimeout int
+}
+
+var serverConfigure *ServerConfigure = &ServerConfigure{}
+
+func Init() {
+	flag.StringVar(&serverConfigure.host, "host", "0.0.0.0", "host for server to listen on")
+	flag.StringVar(&serverConfigure.port, "port", "8612", "port for server to listen on")
+	flag.IntVar(&serverConfigure.readTimeout, "read_timeout", 30, "maximum duration before timing out read of the request (second)")
+	flag.IntVar(&serverConfigure.writeTimeout, "write_timeout", 30, "maximum duration before timing out write of the request (second)")
+	flag.Parse()
+}
+
 func main() {
+	Init()
 	for i, v := range Hosts {
 		fmt.Println(i, v)
 	}
-	http.HandleFunc("/hello", HelloServer)
-	log.Print("server start at: 8612")
-	err := http.ListenAndServe(":8612", nil)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
+	httpServer := http.Server{
+		Addr:         serverConfigure.host + ":" + serverConfigure.port,
+		Handler:      &Router{},
+		ReadTimeout:  time.Duration(serverConfigure.readTimeout) * time.Second,
+		WriteTimeout: time.Duration(serverConfigure.writeTimeout) * time.Second,
 	}
-
+	log.Print("server ready to accept request, configure:")
+	configureValue := reflect.ValueOf(*serverConfigure)
+	configureType := reflect.TypeOf(*serverConfigure)
+	for i := 0; i < configureValue.NumField(); i++ {
+		log.Printf("\t%v:\t%v\n", configureType.Field(i).Name, configureValue.Field(i))
+	}
+	err := httpServer.ListenAndServe()
+	if err != nil {
+		log.Fatal("server down: ", err)
+	}
 }
